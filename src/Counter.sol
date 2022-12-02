4d03af23d47cc3d6781f80e9afc4686ef6ffb620
cd <cleardevice@gmail.com>
2022-12-02 15:27:52 +0200

    Add some tests

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,14 +1,44 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+error NotAnOwner();
+
 contract Counter {
     uint256 public number;
+    address public owner;
 
-    function setNumber(uint256 newNumber) public {
+    event Inc(uint indexed number, address indexed initiator);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "not an owner!");
+        _;
+    }
+
+    modifier onlyOwnerCustomError() {
+        if (msg.sender != owner) {
+            revert NotAnOwner();
+        }
+        _;
+    }
+
+    constructor(uint _initialNum) {
+        number = _initialNum;
+        owner = msg.sender;
+    }
+
+    function setNumber(uint256 newNumber) external onlyOwner {
         number = newNumber;
     }
 
-    function increment() public {
+    function increment() external onlyOwner {
+        number++;
+
+        emit Inc(number, msg.sender);
+    }
+
+    function incrementCustomError() external onlyOwnerCustomError {
         number++;
     }
+
+    receive() external payable {}
 }
--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -2,23 +2,64 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import "../src/Counter.sol";
+import "forge-std/console.sol";
+import { Counter, NotAnOwner } from "../src/Counter.sol";
 
 contract CounterTest is Test {
     Counter public counter;
 
+    event Inc(uint indexed number, address indexed initiator);
+
     function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
+        counter = new Counter(100);
+    }
+
+    function testReceive() public {
+        assertEq(address(counter).balance, 0);
+
+        (bool success,) = address(counter).call{value: 100}("");
+        assertEq(success, true);
+        assertEq(address(counter).balance, 100);
     }
 
     function testIncrement() public {
+        vm.expectEmit(true, true, true, false);
+        emit Inc(101, address(this));
+
         counter.increment();
-        assertEq(counter.number(), 1);
+        assertEq(counter.number(), 101);
     }
 
     function testSetNumber(uint256 x) public {
         counter.setNumber(x);
         assertEq(counter.number(), x);
+    }
+
+    function testFailIncrementNotOwner() public {
+        vm.prank(address(0));
+        counter.increment();
+    }
+
+    function testIncrementNotOwner() public {
+        vm.expectRevert(bytes("not an owner!"));
+        vm.prank(address(0));
+        counter.increment();
+    }
+
+    function testIncrementNotOwnerCustomError() public {
+        vm.expectRevert(NotAnOwner.selector);
+        vm.prank(address(0));
+        counter.incrementCustomError();
+    }
+
+    function testFailSubstraction() public view {
+        uint num = counter.number();
+        num -= 1000;
+    }
+
+    function testSubstractionUnderflow() public {
+        uint num = counter.number();
+        vm.expectRevert(stdError.arithmeticError);
+        num -= 1000;
     }
 }
